a5401d30d89c6c8b3feb061e501cb24cdb577425
1134961980 <1134961980@qq.com>
2021-02-24 15:45:29 +0800

    小米有品-小程序

---
+++ pages/camera/index.js
@@ -0,0 +1,90 @@
+// pages/camera/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isCameraReady:true,//尝试渲染
+    src:""
+  },
+  cameraInitFail(e){
+    console.log(e)
+    this.setData({
+      isCameraReady:false
+    })
+  },
+  takePhone(){
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  openSetting(){
+    wx.openSetting();
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.setData({
+      isCameraReady:true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    this.setData({
+      isCameraReady:false
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
